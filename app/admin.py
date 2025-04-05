e483a2a15672b8ad68b491e6fe72b6246ab6447c
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-04-05 12:36:59 +0530

    added project model

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Task
+from .models import User, Task, Project
 
 # Register your models here.
 
@@ -9,8 +9,15 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "description", "assigned_to","created_by","status"]
+    list_display = ["id", "title", "description", "assigned_to", "created_by", "status"]
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "total_amount"]
+
+
+admin.site.register(
+    Project,ProjectAdmin
+)
 admin.site.register(User, UserAdmin)
 admin.site.register(Task, TaskAdmin)
